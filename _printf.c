6e68191270cc528be3a2882755799345b0785e25
Lovette Duke <qutyluv@gmail.com>
2023-09-17 21:15:43 +0100

    Update _printf.c
    printf.c contribution

--- _printf.c
+++ _printf.c
@@ -38,10 +38,10 @@ int _printf(const char *format, ...)
 		else
 		{
 			_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
+			flags = _flags(format, &i);
+			width = _width(format, &i, list);
+			precision = _precision(format, &i, list);
+			size = _size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
 				flags, width, precision, size);
