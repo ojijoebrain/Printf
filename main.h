04a14594b133367db7b4480cb4a9959736d192d1
Lovette Duke <qutyluv@gmail.com>
2023-09-17 21:05:23 +0100

    Update main.h
    main.h contribution

--- main.h
+++ main.h
@@ -79,7 +79,7 @@ int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
 /* Funciotns to handle other specifiers */
-int get_flags(const char *format, int *i);
+int _flags(const char *format, int *i);
 int _width(const char *format, int *i, va_list list);
 int _precision(const char *format, int *i, va_list list);
 int _size(const char *format, int *i);
