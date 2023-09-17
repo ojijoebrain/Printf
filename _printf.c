3885f6bdadf578c820ecc3b697b78987ae08e79a
Lovette Duke <qutyluv@gmail.com>
2023-09-17 20:31:41 +0100

    Update _printf.c
    additional changes

--- _printf.c
+++ _printf.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
+void _buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - the Printf function
@@ -31,13 +31,13 @@ int _printf(const char *format, ...)
 		{
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
+				_buffer(buffer, &buff_ind);
 			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
+			_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
@@ -51,7 +51,7 @@ int _printf(const char *format, ...)
 		}
 	}
 
-	print_buffer(buffer, &buff_ind);
+	_buffer(buffer, &buff_ind);
 
 	va_end(list);
 
@@ -59,11 +59,11 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - checks if the buffer exist and prints its content
+ * _buffer - checks if the buffer exist and prints its content
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
-void print_buffer(char buffer[], int *buff_ind)
+void _buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
