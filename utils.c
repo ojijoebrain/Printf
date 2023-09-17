909efd594fc094d9528fef701b162c9c3f1b5118
Lovette Duke <qutyluv@gmail.com>
2023-09-17 21:11:03 +0100

    Update utils.c
    utils.c contribution

--- utils.c
+++ utils.c
@@ -2,7 +2,7 @@
 
 /**
  * is_printable - Evaluates if a char is printable
- * @c: Char to be evaluated.
+ * @c: Chararacter to be evaluated.
  *
  * Return: 1 if c is printable, 0 otherwise
  */
@@ -15,15 +15,15 @@ int is_printable(char c)
 }
 
 /**
- * append_hexa_code - Append ascci in hexadecimal code to buffer
- * @buffer: Array of chars.
+ * append_hexa_code - Append ascci characters in hexadecimal code to the buffer
+ * @buffer: Array of characters.
  * @i: Index at which to start appending.
- * @ascii_code: ASSCI CODE.
+ * @ascii_code: ASSCI Code.
  * Return: Always 3
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
-	char map_to[] = "0123456789ABCDEF";
+	char _map[] = "0123456789ABCDEF";
 	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
@@ -31,15 +31,15 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 	buffer[i++] = '\\';
 	buffer[i++] = 'x';
 
-	buffer[i++] = map_to[ascii_code / 16];
-	buffer[i] = map_to[ascii_code % 16];
+	buffer[i++] = _map[ascii_code / 16];
+	buffer[i] = _map[ascii_code % 16];
 
 	return (3);
 }
 
 /**
- * is_digit - Verifies if a char is a digit
- * @c: Char to be evaluated
+ * is_digit - Verifies if a char is an integer
+ * @c: Character to be evaluated
  *
  * Return: 1 if c is a digit, 0 otherwise
  */
@@ -52,8 +52,8 @@ int is_digit(char c)
 }
 
 /**
- * convert_size_number - Casts a number to the specified size
- * @num: Number to be casted.
+ * convert_size_number - converts a number to the specified size
+ * @num: the number to be casted.
  * @size: Number indicating the type to be casted.
  *
  * Return: Casted value of num
@@ -69,9 +69,9 @@ long int convert_size_number(long int num, int size)
 }
 
 /**
- * convert_size_unsgnd - Casts a number to the specified size
- * @num: Number to be casted
- * @size: Number indicating the type to be casted
+ * convert_size_unsgnd - Converts a number to the specified size
+ * @num: the number to be casted
+ * @size: the number indicating the type to be casted
  *
  * Return: Casted value of num
  */
