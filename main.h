7ff93c17fcc3a2ef649d4de2db291cc8f8c544a5
Lovette Duke <qutyluv@gmail.com>
2023-09-18 14:22:44 +0100

    main.h contribution

--- main.h
+++ main.h
@@ -75,7 +75,7 @@ int _non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
 /* Funcion to print memory address */
-int print_pointer(va_list types, char buffer[],
+int _pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
 /* Funciotns to handle other specifiers */
